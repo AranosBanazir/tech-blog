610dd368c2683922c3a4002bf434a8e619e2df21
Caleb Saiia <calebsaiia@gmail.com>
2024-06-03 05:56:00 -0500

    Set up initial file structure

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+require("dotenv").config();
 const express = require(`express`);
+const sequelize = require("./config/connection");
+
 const app = express();
-const PORT = process.env.PORT || 3001
-const routes = require(`./routes`)
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const session = require("express-session");
+const { engine } = require("express-handlebars");
+const routes = require(`./controllers`);
 
-app.use(express.static(`public`))
-app.use(express.json())
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.use(routes);
+app.use(
+  session({
+    secret: "supersecret", //fix later for .env
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
+);
+app.use(express.static(path.join(__dirname, `public`)));
+app.use(express.json());
 
-app.listen(PORT, ()=>{
-console.clear()
-console.log(`Listening on port: ${PORT}`)
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.clear();
+    console.log(`Listening on port: ${PORT}`);
+  });
+});
