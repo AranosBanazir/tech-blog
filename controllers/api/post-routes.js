6af108ea3f1d80485e5767db167e07f8c53a23fa
Caleb Saiia <calebsaiia@gmail.com>
2024-06-08 04:26:03 -0500

    added update/delete post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -32,8 +32,6 @@ router.get("/:id", auth, async (req, res) => {
     ],
   });
 
-  // post = post.get({ plain: true });
-
   res.status(200).json(post);
 });
 
@@ -44,9 +42,59 @@ router.post("/", auth, async (req, res) => {
   };
 
   console.log(newPost);
-  const post = await Post.create(newPost);
+  await Post.create(newPost);
 
   res.status(201).end();
+});
+
+router.put("/:id", auth, async (req, res) => {
+  let post = await Post.findByPk(req.body.post_id);
+  post = post.get({ plain: true });
+
+  //making sure the person changing the post
+  //is the person who made the post
+  if (req.session.user_id !== post.user_id) {
+    res.status(401).end();
+    return;
+  }
+
+  const newPost = await Post.update(
+    {
+      title: req.body.title,
+      body: req.body.body,
+    },
+    {
+      where: {
+        user_id: req.session.user_id,
+        id: req.body.post_id,
+      },
+    }
+  );
+
+  console.log(post);
+  res.status(200).end();
+});
+
+router.delete("/:id", async (req, res) => {
+  let post = await Post.findByPk(req.params.id);
+  post = post.get({ plain: true });
+
+  //making sure the person changing the post
+  //is the person who made the post
+  if (req.session.user_id !== post.user_id) {
+    res.status(401).end();
+    return;
+  }
+
+  const destroyed = await Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  if (destroyed === 1) {
+    res.status(204).end();
+  }
 });
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,25 +7,26 @@ const editTitleInput = document.querySelector("#editTitleInput");
 const editContentInput = document.querySelector("#editContentInput");
 
 const handleEdit = (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
   const { title, body } = e.target.elements;
 
   const newPost = {
     title: title.value,
     body: body.value,
+    post_id: editForm.dataset.id,
   };
   console.log(newPost);
 
-  fetch("/api/posts", {
+  fetch(`/api/posts/${editForm.dataset.id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newPost),
   }).then((res) => {
-    if (res.status === 201) {
-      console.log("WE CHANGED A POST");
+    if (res.status === 200) {
+      location.reload();
     }
   });
 };
@@ -48,35 +49,56 @@ const handleNew = (e) => {
     body: JSON.stringify(newPost),
   }).then((res) => {
     if (res.status === 201) {
-      console.log("WE MADE A NEW POST");
+      location.reload();
     }
   });
 };
 
-const showEditor = () => {};
-
 const handlePost = async (e) => {
-  if (!e.target.dataset.id) return;
+  if (e.target.dataset.id) {
+    editForm.classList.remove("d-none");
+    if (!newForm.classList.contains("d-none")) {
+      newForm.classList.add("d-none");
+    }
 
-  editForm.classList.remove("d-none");
+    await fetch(`/api/posts/${e.target.dataset.id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        editTitleInput.value = data.title;
+        editContentInput.innerHTML = data.body;
+        editForm.dataset.id = data.id;
+      });
+  } else if (e.target.dataset.delete) {
+    //hiding the editor on delete
+    if (!editForm.classList.contains("d-none")) {
+      editForm.classList.add("d-none");
+    }
 
-  await fetch(`/api/posts/${e.target.dataset.id}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      editTitleInput.value = data.title;
-      editContentInput.innerHTML = data.body;
+    await fetch(`/api/posts/${e.target.dataset.delete}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      if (res.status === 204) {
+        location.reload();
+      }
     });
+  }
 };
 
-
-
-
-
-
+const showNewEditor = () => {
+  if (!editForm.classList.contains("d-none")) {
+    editForm.classList.add("d-none");
+  }
+  if (newForm.classList.contains("d-none")) {
+    newForm.classList.remove("d-none");
+  }
+};
 
 editForm.addEventListener("submit", handleEdit);
 newForm.addEventListener("submit", handleNew);
-newBtn.addEventListener("click", showEditor);
+newBtn.addEventListener("click", showNewEditor);
 postContainer.addEventListener("click", handlePost);
